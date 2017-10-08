e7756ed97a3074e988587b0636d02902196e5d71
Dylan Cross <dylancross@me.com>
2017-10-08 19:50:00 +1300

    Set up database + dummy values

--- MVCexample/web/src/model/Model.php
+++ MVCexample/web/src/model/Model.php
@@ -44,8 +44,11 @@ class Model
             error_log("Mysql database not available!",0);
         }
 
-        $result = $this->db->query("SHOW TABLES LIKE 'account';");
 
+        /**
+         * account table
+         */
+        $result = $this->db->query("SHOW TABLES LIKE 'account';");
         if ($result->num_rows == 0) {
             // table doesn't exist
             // create it and populate with sample data
@@ -53,7 +56,10 @@ class Model
             $result = $this->db->query(
                                 "CREATE TABLE `account` (
                                           `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
-                                          `name` varchar(256) DEFAULT NULL,
+                                          `name` varchar(256) NOT NULL,
+                                          `username` varchar(256) NOT NULL,
+                                          `email` varchar(256) NOT NULL,
+                                          `password` varchar(256) NOT NULL,
                                           PRIMARY KEY (`id`) );"
             );
 
@@ -63,13 +69,49 @@ class Model
             }
 
             if(!$this->db->query(
-                "INSERT INTO `account` VALUES (NULL,'Bob'), (NULL,'Mary');"
+                "INSERT INTO `account` VALUES (NULL,'Dylan', 'dylan', 'dylan@example.com', '123'),
+                                              (NULL,'Jordan', 'jordan', 'jordan@example.com', '123');"
+            )) {
+                // handle appropriately
+                error_log("Failed creating sample data!",0);
+            }
+        }
+
+        /**
+         * product table
+         */
+        $result = $this->db->query("SHOW TABLES LIKE 'product';");
+        if ($result->num_rows == 0) {
+            // table doesn't exist
+            // create it and populate with sample data
+
+            $result = $this->db->query(
+                "CREATE TABLE `product` (
+                                          `id` int(8) unsigned NOT NULL AUTO_INCREMENT,
+                                          `sku` varchar(256) NOT NULL,
+                                          `name` varchar(256) NOT NULL,
+                                          `cost` float(8,2) NOT NULL,
+                                          `stock` int(8) NOT NULL,
+                                          PRIMARY KEY (`id`) );"
+            );
+
+            if (!$result) {
+                // handle appropriately
+                error_log("Failed creating table account",0);
+            }
+
+            if(!$this->db->query(
+                "INSERT INTO `product` VALUES (NULL,'ham11', 'Claw Hammer', 39.95, 11),
+                                              (NULL,'ham22', 'Sledge Hammer', 66.00, 2),
+                                              (NULL,'ham23', 'Soft-Face Hammer', 24.99, 7),
+                                              (NULL,'screw03', 'Flat Hammer', 11.95, 25),
+                                              (NULL,'ham22', 'Phillips Hammer', 11.95, 30);"
+
             )) {
                 // handle appropriately
                 error_log("Failed creating sample data!",0);
             }
         }
         //----------------------------------------------------------------------------
-
     }
 }
