dd78160ac6450b624b9dfafecf766bd0a5569616
Dylan Cross <dylancross@me.com>
2017-10-17 12:20:18 +1300

    Added more dummy products

--- MVCexample/web/src/model/Model.php
+++ MVCexample/web/src/model/Model.php
@@ -70,7 +70,8 @@ class Model
 
             if(!$this->db->query(
                 "INSERT INTO `account` VALUES (NULL,'Dylan', 'dylan', 'dylan@example.com', '123'),
-                                              (NULL,'Jordan', 'jordan', 'jordan@example.com', '123');"
+                                              (NULL,'Jordan', 'jordan', 'jordan@example.com', '123'),
+                                              (NULL, 'Tim Taylor', 'tim@thetoolman.com', 'TheToolman', 'TheToolman');"
             )) {
                 // handle appropriately
                 error_log("Failed creating sample data!",0);
@@ -105,8 +106,30 @@ class Model
                 "INSERT INTO `product` VALUES (NULL,'ham11', 'Claw Hammer', 39.95, 'hammers', 11),
                                               (NULL,'ham22', 'Sledge Hammer', 66.00, 'hammers', 2),
                                               (NULL,'ham23', 'Soft-Face Hammer', 24.99, 'hammers', 7),
-                                              (NULL,'screw03', 'Flat Hammer', 11.95, 'hammers', 25),
-                                              (NULL,'ham22', 'Phillips Hammer', 11.95, 'hammers', 30);"
+                                              (NULL,'ham23', 'Full Size Wrecking Bar', 45.99, 'hammers', 2),
+                                              (NULL,'ham23', 'mini Wrecking Bar', 24.99, 'hammers', 2),
+                                              (NULL,'screw01', 'Flat Head Screwdriver', 11.95, 'screwdrivers', 17),
+                                              (NULL,'screw02', 'Phillips Head Screwdrivers', 11.95, 'screwdrivers', 13),
+                                              (NULL,'screw03', 'Square Head Screwdrivers', 11.95, 'screwdrivers', 11),
+                                              (NULL,'screw04', 'Torx Head Screwdrivers', 11.95, 'screwdrivers', 15),
+                                              (NULL,'screw11', 'Flat Head Screwdriver VDE', 29.99, 'screwdrivers', 2),
+                                              (NULL,'screw12', 'Phillips Head Screwdrivers VDE', 29.99, 'screwdrivers', 3),
+                                              (NULL,'screw13', 'Square Head Screwdrivers VDE', 29.99, 'screwdrivers', 1),
+                                              (NULL,'screw14', 'Torx Head Screwdrivers VDE', 29.99, 'screwdrivers', 2),
+                                              (NULL,'screw14', 'Torx Head Screwdrivers VDE', 29.99, 'screwdrivers', 0),
+                                              (NULL,'screw00', '4 Pce Screwdriver Set', 69.00, 'screwdrivers', 2),
+                                              (NULL,'screw30', '9 Pce Hex Allen Key Set', 32.00, 'screwdrivers', 3),
+                                              (NULL,'plier01', 'Linesman Pliers', 5.95, 'pliers', 3),
+                                              (NULL,'plier02', 'Long Nose Pliers', 5.95, 'pliers', 3),
+                                              (NULL,'plier03', 'Diagonal Pliers', 5.95, 'pliers', 3),
+                                              (NULL,'plier04', 'Groove Joint Pliers', 5.95, 'pliers', 3),
+                                              (NULL,'plier05', 'End Nipper Pliers', 5.95, 'pliers', 3),
+                                              (NULL,'saw01', 'Hardpoint Saw', 40.00, 'saws', 17),
+                                              (NULL,'saw02', 'Insulation Saw', 20.00, 'saws', 2),
+                                              (NULL,'saw03', 'Dovetail Saw', 35.00, 'saws', 10),
+                                              (NULL,'saw04', 'Pull Saw', 30.00, 'saws', 7),
+                                              (NULL,'saw05', 'Toolbox Saw', 30.00, 'saws', 7);"
+
 
             )) {
                 // handle appropriately
